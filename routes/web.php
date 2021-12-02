e8fa20b1bfb62b664672dfaa2c3615f5acb10fe8
yasushi miyazaki <86082978+zaq-worker@users.noreply.github.com>
2021-12-02 13:07:09 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('create');
+    return view('index');
 });
 
 Route::get('/userdata', 'App\Http\Controllers\UserdataController@index')->name('userdata.list'); 
