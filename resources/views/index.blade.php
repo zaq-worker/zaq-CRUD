0ab689c80033dacbf212de1ce3fa48b40789c8f0
yasushi miyazaki <86082978+zaq-worker@users.noreply.github.com>
2021-12-16 12:49:09 +0900

    テーブル 横スクロール対応

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,7 +24,7 @@
                                     <p class="col-sm-12 col-md-10 col-lg-7 container text-right">
                                         <a href="{{ route('userdata.create') }}">{{ __('>>登録') }}</a>
                                     </p>
-                            <table class="table table-bordered col-sm-12 col-md-10 col-lg-7 container-fluid text-center">
+                            <table class="table table-bordered table-responsive col-sm-12 col-md-10 col-lg-7 container-fluid text-center">
                                 <tr>  
                                     <th>名前</th>
                                     <th>電話番号</th>
