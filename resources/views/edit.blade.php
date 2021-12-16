7e7ae4eac69b5444f1e50a6bd57bd454a483bd51
yasushi miyazaki <86082978+zaq-worker@users.noreply.github.com>
2021-12-16 12:46:35 +0900

    footer 幅調整

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -43,7 +43,7 @@
                 </div>
             </div>
         </div>
-        <footer style="position:absolute; bottom:0;">
+        <footer style="position:absolute; bottom:0; width: 100%;">
             <hr/>
             <div class="container-fluid text-center">
                 <p class="help-block"> ©CRUD.com 2021 </p>
